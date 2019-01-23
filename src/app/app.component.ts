1b6447f004046354f06ccdb59a71d4ead6830e3d
unknown <abhina663445@majesco.com>
2019-01-23 09:48:12 +0000

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SimulatorService } from './simulator.service';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,58 @@ export class AppComponent implements OnInit {
   acceleration_x: any = 0;
   acceleration_y: any = 0;
   acceleration_z: any = 0;
+  totalAcceleration: any = 0;
+  readonly VAPID_PUBLIC_KEY =
+    'BAonEp9mCq5JkdVC8zK9lPZDhHeFaeDLel1OFUzgOGOW7cTVscv8w9l8KQSgqhBBX0s6_oaz8ROyOxPg9IntTtc';
+  subscribed = false;
   /**
    *
    */
-  constructor(private _simulator: SimulatorService) {}
+  constructor(private swPush: SwPush, private _simulator: SimulatorService) {}
   ngOnInit() {
+    console.log('subscribed', this.subscribed);
+  }
+
+  subscribeToNotifications() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(sub => {
+        console.log(sub);
+        this._simulator.addPushSubscriber(sub).subscribe(res => {
+          // console.log('hala lua ');
+          this.subscribed = true; //  flag for subscription
+          this.attachEventlistner();
+        });
+      })
+      .catch(err => console.error('Could not subscribe to notifications', err));
+  }
+  attachEventlistner() {
+    console.log('callling attach event listner');
+
     window.addEventListener('devicemotion', event => {
       console.log('event triggered');
-
       this.acceleration_x = event.acceleration.x + ' m/s2';
       this.acceleration_y = event.acceleration.y + ' m/s2';
       this.acceleration_z = event.acceleration.z + ' m/s2';
-      this._simulator.sendData();
+      const avgAcceleration = Math.sqrt(
+        Math.pow(event.acceleration.x, 2) +
+          Math.pow(event.acceleration.y, 2) +
+          Math.pow(event.acceleration.z, 2)
+      );
+      // this._simulator.sendAccelerationData('98').subscribe(res => {
+      //   console.log(res);
+      // });
+      if (Math.abs(this.totalAcceleration - avgAcceleration) > 0.5) {
+        this.totalAcceleration = avgAcceleration;
+        console.log('calling send accelerate method ');
+        this._simulator
+          .sendAccelerationData(this.totalAcceleration)
+          .subscribe(res => {
+            console.log(res);
+          });
+      }
     });
   }
 }
--- src/app/simulator.service.ts
+++ src/app/simulator.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimulatorService {
+  subscribed = false;
   constructor(private http: HttpClient) {}
   sendData() {
     console.log('calling service');
@@ -14,10 +15,39 @@ export class SimulatorService {
     //   .subscribe(res => {
     //     // console.log(res);
     //   });
-    this.http
-      .get('https://52.229.8.80:3000')
-      .subscribe(res => {
-        // console.log(res);
-      });
+    this.http.get('http://52.229.8.80:8085/users').subscribe(res => {
+      // console.log(res);
+    });
+  }
+
+  addPushSubscriber(sub: any) {
+    console.log(sub);
+
+    // return this.http.post('http://localhost:3000/users/newsletter', sub);
+    // return this.http.post('http://52.229.8.80:8085/users/newsletter', sub);
+    return this.http.post(
+      'https://cloudcartup.xyz/node/NodeEndpoint/users/newsletter',
+      sub
+    );
+  }
+
+  sendAccelerationData(acceleration: string) {
+    console.log('calling service');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post(
+      // 'http://localhost:3000/users/sendAcceleration',
+      // 'http://52.229.8.80:8085/users/sendAcceleration',
+      'https://cloudcartup.xyz/node/NodeEndpoint/users/sendAcceleration',
+      { acceleration: acceleration },
+      httpOptions
+    );
   }
 }
+// 'http://52.229.8.80:8085/users/sendAcceleration',
+// 'http://localhost:3000/users/sendAcceleration',
+// https://cloudcartup.xyz/node/NodeEndpoint/users
